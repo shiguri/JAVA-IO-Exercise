221f1b4bcfc9823919081614785e6b6aba3187fe
shiguri <yeguangpeng2018@outlook.com>
2022-05-27 10:04:09 +0800

    WebServer

--- NIO-SERVER-Demo/src/main/java/com/ygp/demo/Client.java
+++ NIO-SERVER-Demo/src/main/java/com/ygp/demo/Client.java
@@ -3,31 +3,97 @@ package com.ygp.demo;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    private final static int WRITE_CAPACITY = 1024;
+    private final static int READ_CAPACITY = 1024;
 
-        SocketChannel socketChannel = SocketChannel.open();
-        socketChannel.connect(new InetSocketAddress("127.0.0.1", 8081));
+    private Selector selector;
+    // 不同于Server， Client 只需要维持一个 SocketChannel就可以了。
+    private SocketChannel socket;
 
-        ByteBuffer writeBuffer = ByteBuffer.allocate(32);
-        ByteBuffer readBuffer = ByteBuffer.allocate(32);
-
-        writeBuffer.put("hello".getBytes());
-        writeBuffer.flip();
-
-        while (true) {
-            writeBuffer.rewind();
-            socketChannel.write(writeBuffer);
-            readBuffer.clear();
-            socketChannel.read(readBuffer);
-            try {
-                Thread.sleep(10000);
-            }catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    public Client() {
+        try {
+            this.selector = Selector.open();
+        }catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
+    public void clientConnectInit(String address, int port) throws IOException {
+        socket = SocketChannel.open();
+        socket.connect(new InetSocketAddress(address, port));
+        socket.configureBlocking(false);
+        // 该 Channel 在 read 事件时触发。
+        socket.register(selector, SelectionKey.OP_WRITE);
+    }
+
+    public void write(SelectionKey key, String data) throws IOException{
+        ByteBuffer buffer = ByteBuffer.allocate(WRITE_CAPACITY);
+        buffer.clear();
+        buffer.put(data.getBytes("UTF_16"));
+        buffer.flip();
+        SocketChannel channel = (SocketChannel) key.channel();
+        channel.write(buffer);
+        key.interestOps(SelectionKey.OP_READ);
+    }
+
+    public String read(SelectionKey key) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(READ_CAPACITY);
+        SocketChannel channel = (SocketChannel) key.channel();
+
+        int count = channel.read(buffer);
+        // 没有数据
+        if ( -1 == count) {
+            key.cancel();
+            return "";
+        }
+        buffer.flip();
+        String result = buffer.asCharBuffer().toString();
+        key.interestOps(SelectionKey.OP_WRITE);
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        final Client client = new Client();
+        client.clientConnectInit("127.0.0.1", 8082);
+
+        /**
+         * 创建辅助线程，负责读取 Server 通信。
+         */
+        Thread readThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while(true) {
+                        int nReady = client.selector.select();
+                        Set<SelectionKey> keys = client.selector.selectedKeys();
+                        Iterator<SelectionKey> iterator = keys.iterator();
+                        int index = 1;
+
+                        while (iterator.hasNext()) {
+                            SelectionKey event = iterator.next();
+                            iterator.remove();
+                            if (event.isValid()) {
+                                if (event.isReadable()) {
+                                    String s = client.read(event);
+                                    System.out.println("Server : " + s);
+                                } else if (event.isWritable()) {
+                                    client.write(event, index + " data");
+                                    index += 1 ;
+                                }
+                            }
+
+                        }
+                    }
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        readThread.start();
     }
 }
--- NIO-SERVER-Demo/src/main/java/com/ygp/demo/WebServer.java
+++ NIO-SERVER-Demo/src/main/java/com/ygp/demo/WebServer.java
@@ -3,66 +3,96 @@ package com.ygp.demo;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.Iterator;
 import java.util.Set;
 
 public class WebServer {
-    private final static String host = "127.0.0.1";
-    private final static int port = 8081;
     private final static int readCapacity = 1024;
     private final static int writeCapacity = 1024;
 
-    public static void main(String[] args) throws IOException {
-        Selector selector = Selector.open();
+    private Selector selector;
+    private ByteBuffer readBuffer;
+    private ByteBuffer writeBuffer;
 
-        /**
-         * 创建一个 ServerSocketChannel 并注册到Selector中。
-         * 指定的触发事件为 ACCEPT，即一个新的连接请求。
-         */
+    public WebServer() {
+        try {
+            selector = Selector.open();
+            readBuffer = ByteBuffer.allocate(readCapacity);
+            writeBuffer = ByteBuffer.allocate(writeCapacity);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void runServer(String address, int port) throws IOException {
         ServerSocketChannel ssc = ServerSocketChannel.open();
-        ssc.socket().bind(new InetSocketAddress(host, port));
         ssc.configureBlocking(false);
-        ssc.register(selector, SelectionKey.OP_ACCEPT);
-
-        ByteBuffer readBuffer = ByteBuffer.allocate(readCapacity);
-        ByteBuffer writeBuffer = ByteBuffer.allocate(writeCapacity);
+        ssc.bind(new InetSocketAddress(address, port));
+        ssc.register(this.selector, SelectionKey.OP_ACCEPT);
 
         while(true) {
-            int nReady = selector.select();
-            Set<SelectionKey> selectionKeys = selector.selectedKeys();
-            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            int nReady = this.selector.select();
+            Set<SelectionKey> keys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = keys.iterator();
             while(iterator.hasNext()) {
                 SelectionKey event = iterator.next();
                 iterator.remove();
 
-                if (event.isAcceptable()) {
-                    /**
-                     * 创建一个新的连接，并将新连接绑定到 Selector 上，
-                     * 新连接只对 read 事件感兴趣。
-                     */
-                    SocketChannel newSocketChannel = ssc.accept();
-                    newSocketChannel.configureBlocking(false);
-                    newSocketChannel.register(selector, SelectionKey.OP_READ);
-                } else if (event.isReadable()) {
-                    SocketChannel socketChannel = (SocketChannel) event.channel();
-                    readBuffer.clear();
-                    socketChannel.read(readBuffer);
-                    readBuffer.flip();
-                    System.out.println( new String(readBuffer.array()) );
-                    event.interestOps(SelectionKey.OP_WRITE);
-                } else if (event.isWritable()) {
-                    SocketChannel socketChannel = (SocketChannel) event.channel();
-                    writeBuffer.rewind();
-                    socketChannel.write(writeBuffer);
-                    event.interestOps(SelectionKey.OP_READ);
+                if (event.isValid()) {
+                    if(event.isAcceptable()) {
+                        this.accept(event);
+                    } else if (event.isReadable()) {
+                        String str = this.read(event);
+                        System.out.println("Client : " + str);
+                    } else if (event.isWritable()) {
+                        String data = "received";
+                        this.write(event ,data);
+                    }
                 }
-
             }
         }
     }
 
+    public void accept(SelectionKey key) throws IOException {
+        ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+        SocketChannel newChannel = ssc.accept();
+        newChannel.configureBlocking(false);
+        newChannel.register(this.selector, SelectionKey.OP_READ);
+    }
+
+    public String read(SelectionKey key) throws IOException {
+        SocketChannel channel = (SocketChannel)key.channel();
+        readBuffer.clear();
+        channel.read(readBuffer);
+        readBuffer.flip();
+
+        // 读完后，注册感兴趣事件为 Write
+        channel.register(selector, SelectionKey.OP_WRITE);
+        return readBuffer.asCharBuffer().toString();
+    }
+
+    public void write(SelectionKey key, String data) throws IOException{
+        SocketChannel channel = (SocketChannel) key.channel();
+        writeBuffer.clear();
+        writeBuffer.put(data.getBytes("UTF_16"));
+        writeBuffer.flip();
+        channel.write(writeBuffer);
+
+        key.channel().close();
+        key.cancel();
+    }
+
+    public static void main(String[] args) {
+        WebServer server = new WebServer();
+        final int port = 8082;
+        final String address = "127.0.0.1";
+        try {
+            server.runServer(address, port);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
